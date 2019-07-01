a6123291eec19634980caa14d693a988f19e7ba9
Edwin <Edwin.Martinez@people10.com>
2019-06-30 23:08:19 -0400

    Added connection code to MongoDB

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 
+const { dbUser, dbPass, tks } = require('../config/config');
+
+mongoose.connect("mongodb://"+dbUser+":"+dbPass+"@ds345597.mlab.com:45597/stint_v2")
+  .then(() => {
+    console.log("connected to mongoDB")
+  },
+  err => {
+    console.log("err", err)
+  }
+  );
+mongoose.Promise = global.Promise;
 
 
 module.exports = router;
