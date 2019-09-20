3bf3371cc4c05bb9dd6eb4fc3dd34c1115e68c10
Edwin <Edwin.Martinez@people10.com>
2019-09-20 16:14:14 -0400

    edited gitignore

---
+++ client/build/precache-manifest.de220a206caf14a92cadb80f8c6149be.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ea25156207e6893c0a7dd002239999",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4556b5f10f9aab5d3db1",
+    "url": "/static/css/2.8ba90912.chunk.css"
+  },
+  {
+    "revision": "ef0002828f5fe753096d",
+    "url": "/static/css/main.92c29b52.chunk.css"
+  },
+  {
+    "revision": "4556b5f10f9aab5d3db1",
+    "url": "/static/js/2.dd221250.chunk.js"
+  },
+  {
+    "revision": "ef0002828f5fe753096d",
+    "url": "/static/js/main.f726631a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f4c5dce250fcece6022386d6a9bc47bc",
+    "url": "/static/media/clean.f4c5dce2.jpg"
+  },
+  {
+    "revision": "77f41cfac4ca3ab70a45a1d38e5a0d7b",
+    "url": "/static/media/clothespin.77f41cfa.jpg"
+  },
+  {
+    "revision": "0110ea6ff4c80a488dc6deca09208a88",
+    "url": "/static/media/clutter-2.0110ea6f.jpg"
+  }
+]);
